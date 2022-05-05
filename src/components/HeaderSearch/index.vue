43386f41a6142d6c9d337a24bc24cda0c567d281
poicc <2631315464@qq.com>
2022-05-05 15:18:27 +0800

    feat: 生成searchPool 渲染检索数据

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -17,25 +17,35 @@
       @change="onSelectChange"
     >
       <el-option
-        v-for="option in 5"
-        :key="option"
-        :label="option"
-        :value="option"
-      ></el-option>
+        v-for="option in searchOptions"
+        :key="option.item.path"
+        :label="option.item.title.join(' > ')"
+        :value="option.item"
+      >
+      </el-option>
     </el-select>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { filterRouters, generateMenus } from '@/utils/route'
-import { useRouter } from 'vue-router'
+import { computed, ref } from 'vue'
+import { generateRoutes } from './FuseData'
 import Fuse from 'fuse.js'
+import { filterRouters } from '@/utils/route'
+import { useRouter } from 'vue-router'
+
+// 检索数据源
+const router = useRouter()
+const searchPool = computed(() => {
+  const filterRoutes = filterRouters(router.getRoutes())
+  return generateRoutes(filterRoutes)
+})
+console.log(searchPool)
 
 /**
  * 搜索库相关
  */
-const fuse = new Fuse(list, {
+const fuse = new Fuse(searchPool.value, {
   // 是否按优先级进行排序
   shouldSort: true,
   // 匹配长度超过这个值的才会被认为是匹配的
@@ -55,15 +65,6 @@ const fuse = new Fuse(list, {
   ]
 })
 
-// 检索数据源
-const router = useRouter()
-const searchPool = computed(() => {
-  const filterRoutes = filterRouters(router.getRoutes())
-  console.log(generateMenus(filterRoutes))
-  return generateMenus(filterRoutes)
-})
-console.log(searchPool)
-
 // 控制 search 显示
 const isShow = ref(false)
 // el-select 实例
@@ -75,13 +76,19 @@ const onShowClick = () => {
 
 // search 相关
 const search = ref('')
+// 搜索结果
+const searchOptions = ref([])
 // 搜索方法
-const querySearch = () => {
-  console.log('querySearch')
+const querySearch = (query) => {
+  if (query !== '') {
+    searchOptions.value = fuse.search(query)
+  } else {
+    searchOptions.value = []
+  }
 }
 // 选中回调
-const onSelectChange = () => {
-  console.log('onSelectChange')
+const onSelectChange = (val) => {
+  router.push(val.path)
 }
 </script>
 
