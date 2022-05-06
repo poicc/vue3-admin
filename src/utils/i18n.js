2a3ca9941517b61012c45eac3c056302148aea7f
poicc <2631315464@qq.com>
2022-05-06 19:07:02 +0800

    feat: 创建tags数据源

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,5 +1,10 @@
+import i18n from '@/i18n'
 import { watch } from 'vue'
 import store from '@/store'
+
+export function generateTitle(title) {
+  return i18n.global.t('msg.route.' + title)
+}
 
 /**
  *
