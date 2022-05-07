4a4073141c2c868e240a26fb2f34b8d730422385
poicc <2631315464@qq.com>
2022-05-07 22:32:23 +0800

    feat: 辅助业务之用户删除

--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -3,7 +3,7 @@
     <el-card class="header">
       <div>
         <el-button type="primary" @click="onImportExcelClick"> {{ $t('msg.excel.importExcel') }}</el-button>
-        <el-button type="success">
+        <el-button type="success" @click="onToExcelClick">
           {{ $t('msg.excel.exportExcel') }}
         </el-button>
       </div>
@@ -11,25 +11,17 @@
     <el-card>
       <el-table :data="tableData" border style="width: 100%">
         <el-table-column label="#" type="index" />
-        <el-table-column prop="username" :label="$t('msg.excel.name')">
-        </el-table-column>
-        <el-table-column prop="mobile" :label="$t('msg.excel.mobile')">
-        </el-table-column>
+        <el-table-column prop="username" :label="$t('msg.excel.name')"> </el-table-column>
+        <el-table-column prop="mobile" :label="$t('msg.excel.mobile')"> </el-table-column>
         <el-table-column :label="$t('msg.excel.avatar')" align="center">
           <template v-slot="{ row }">
-            <el-image
-              class="avatar"
-              :src="row.avatar"
-              :preview-src-list="[row.avatar]"
-            ></el-image>
+            <el-image class="avatar" :src="row.avatar" :preview-src-list="[row.avatar]"></el-image>
           </template>
         </el-table-column>
         <el-table-column :label="$t('msg.excel.role')">
           <template #default="{ row }">
             <div v-if="row.role && row.role.length > 0">
-              <el-tag v-for="item in row.role" :key="item.id" size="mini">{{
-                item.title
-              }}</el-tag>
+              <el-tag v-for="item in row.role" :key="item.id" size="mini">{{ item.title }}</el-tag>
             </div>
             <div v-else>
               <el-tag size="mini">{{ $t('msg.excel.defaultRole') }}</el-tag>
@@ -41,21 +33,11 @@
             {{ $filters.dateFilter(row.openTime) }}
           </template>
         </el-table-column>
-        <el-table-column
-          :label="$t('msg.excel.action')"
-          fixed="right"
-          width="260"
-        >
+        <el-table-column :label="$t('msg.excel.action')" fixed="right" width="260">
           <template #default>
-            <el-button type="primary" size="mini">{{
-              $t('msg.excel.show')
-            }}</el-button>
-            <el-button type="info" size="mini">{{
-              $t('msg.excel.showRole')
-            }}</el-button>
-            <el-button type="danger" size="mini">{{
-              $t('msg.excel.remove')
-            }}</el-button>
+            <el-button type="primary" size="mini">{{ $t('msg.excel.show') }}</el-button>
+            <el-button type="info" size="mini">{{ $t('msg.excel.showRole') }}</el-button>
+            <el-button type="danger" size="mini" @click="onRemoveClick(row)">{{ $t('msg.excel.remove') }}</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -72,6 +54,8 @@
       >
       </el-pagination>
     </el-card>
+
+    <export-to-excel v-model="exportToExcelVisible"></export-to-excel>
   </div>
 </template>
 
@@ -80,6 +64,8 @@ import { ref } from 'vue'
 import { getUserManageList } from '@/api/user-manage'
 import { watchSwitchLang } from '@/utils/i18n'
 import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import ExportToExcel from './components/Export2Excel.vue'
 
 // 数据相关
 const tableData = ref([])
@@ -115,6 +101,7 @@ const handleCurrentChange = (currentPage) => {
   page.value = currentPage
   getListData()
 }
+
 const router = useRouter()
 /**
  * excel 导入点击事件
@@ -123,6 +110,28 @@ const onImportExcelClick = () => {
   router.push('/user/import')
 }
 
+/**
+ * 删除按钮点击事件
+ */
+const i18n = useI18n()
+const onRemoveClick = (row) => {
+  ElMessageBox.confirm(i18n.t('msg.excel.dialogTitle1') + row.username + i18n.t('msg.excel.dialogTitle2'), {
+    type: 'warning'
+  }).then(async () => {
+    await deleteUser(row._id)
+    ElMessage.success(i18n.t('msg.excel.removeSuccess'))
+    // 重新渲染数据
+    getListData()
+  })
+}
+
+/**
+ * excel 导出点击事件
+ */
+const exportToExcelVisible = ref(false)
+const onToExcelClick = () => {
+  exportToExcelVisible.value = true
+}
 </script>
 
 <style lang="scss" scoped>
