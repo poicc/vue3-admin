ed0aafba9a888c6abaaccd3452bb38522c8c894f
poicc <2631315464@qq.com>
2022-05-07 21:57:51 +0800

    feat: 提供两种文件导入形式

--- src/views/user-manage/index.vue
+++ src/views/user-manage/index.vue
@@ -2,7 +2,7 @@
   <div class="user-manage-container">
     <el-card class="header">
       <div>
-        <el-button type="primary"> {{ $t('msg.excel.importExcel') }}</el-button>
+        <el-button type="primary" @click="onImportExcelClick"> {{ $t('msg.excel.importExcel') }}</el-button>
         <el-button type="success">
           {{ $t('msg.excel.exportExcel') }}
         </el-button>
@@ -79,6 +79,7 @@
 import { ref } from 'vue'
 import { getUserManageList } from '@/api/user-manage'
 import { watchSwitchLang } from '@/utils/i18n'
+import { useRouter } from 'vue-router'
 
 // 数据相关
 const tableData = ref([])
@@ -114,6 +115,14 @@ const handleCurrentChange = (currentPage) => {
   page.value = currentPage
   getListData()
 }
+const router = useRouter()
+/**
+ * excel 导入点击事件
+ */
+const onImportExcelClick = () => {
+  router.push('/user/import')
+}
+
 </script>
 
 <style lang="scss" scoped>
