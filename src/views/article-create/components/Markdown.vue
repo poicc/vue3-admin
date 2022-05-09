fb933084e537da93c1552c8fc2853183ca9d0566
poicc <2631315464@qq.com>
2022-05-09 19:33:47 +0800

    feat: markdown文章提交

---
+++ src/views/article-create/components/Markdown.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="markdown-container">
+    <!-- 渲染区 -->
+    <div id="markdown-box"></div>
+    <div class="bottom">
+      <el-button type="primary" @click="onSubmitClick">{{
+        $t('msg.article.commit')
+      }}</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import MkEditor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import '@toast-ui/editor/dist/i18n/zh-cn'
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+import { watchSwitchLang } from '@/utils/i18n'
+import { commitArticle } from './commit'
+// Editor实例
+let mkEditor
+// 处理离开页面切换语言导致 dom 无法被获取
+let el
+onMounted(() => {
+  el = document.querySelector('#markdown-box')
+  initEditor()
+})
+
+const store = useStore()
+const initEditor = () => {
+  mkEditor = new MkEditor({
+    el,
+    height: '500px',
+    previewStyle: 'vertical',
+    language: store.getters.language === 'zh' ? 'zh-CN' : 'en'
+  })
+
+  mkEditor.getMarkdown()
+}
+
+const props = defineProps({
+  title: {
+    required: true,
+    type: String
+  }
+})
+
+const emits = defineEmits(['onSuccess'])
+// 处理提交
+const onSubmitClick = async () => {
+  // 创建文章
+  const res = await commitArticle({
+    title: props.title,
+    content: mkEditor.getHTML()
+  })
+  alert(JSON.stringify(res))
+  await commitArticle({
+    title: props.title,
+    content: mkEditor.getHTML()
+  })
+
+  mkEditor.reset()
+  emits('onSuccess')
+}
+
+watchSwitchLang(() => {
+  if (!el) return
+  const htmlStr = mkEditor.getHTML()
+  mkEditor.destroy()
+  initEditor()
+  mkEditor.setHTML(htmlStr)
+})
+</script>
+
+<style lang="scss" scoped>
+.markdown-container {
+  .bottom {
+    margin-top: 20px;
+    text-align: right;
+  }
+}
+</style>
--- src/views/article-create/index.vue
+++ src/views/article-create/index.vue
@@ -11,7 +11,7 @@
       </el-input>
       <el-tabs v-model="activeName">
         <el-tab-pane :label="$t('msg.article.markdown')" name="markdown">
-          <markdown></markdown>
+          <markdown @onSuccess="onSuccess"></markdown>
         </el-tab-pane>
         <el-tab-pane :label="$t('msg.article.richText')" name="editor">
           <editor></editor>
@@ -25,7 +25,24 @@
 import Editor from './components/Editor.vue'
 import Markdown from './components/Markdown.vue'
 import { ref } from 'vue'
+import { articleDetail } from '@/api/article'
 
+// 处理编辑相关
+const route = useRoute()
+const articleId = route.params.id
+const detail = ref({})
+const getArticleDetail = async () => {
+  detail.value = await articleDetail(articleId)
+  // 标题赋值
+  title.value = detail.value.title
+}
+if (articleId) {
+  getArticleDetail()
+}
+// 创建成功
+const onSuccess = () => {
+  title.value = ''
+}
 const activeName = ref('markdown')
 const title = ref('')
 </script>
